7ea2f486026a772bc9a852698f3424570ded5c2e
Mohsin Nisar <mohsin7007@hotmail.com>
2021-06-10 19:08:35 +0400

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,7 @@ apt-get update -y
 apt-get install -y apache2 libapache2-mod-php php-mysql php-mbstring
 apt install mysql-client-core-8.0
 rm /var/www/html/index.html
-sudo apt install nfs-common
+sudo apt install nfs-common -y
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html
-echo "fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html/ efs defaults,_netdev 0 0" >> /etc/fstab
+grep -qxF 'fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html/ efs defaults,_netdev 0 0' /etc/fstab || echo 'fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html/ efs defaults,_netdev 0 0' >> /etc/fstab
 
