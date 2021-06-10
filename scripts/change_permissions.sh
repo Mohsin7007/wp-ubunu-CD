f301b9d9729f4aa041ba297f0522a18d0b2aefba
Mohsin Nisar <mohsin7007@hotmail.com>
2021-06-10 15:18:37 +0400

    Update change_permissions.sh

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html
-chmod -R 755 /var/www/html/
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html/
+chmod -R 755 /var/www/html
