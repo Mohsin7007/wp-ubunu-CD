a38370fdc5abbcf465d6ed0ef64692dfd35a19a5
Mohsin Nisar <mohsin@integratech.ae>
2021-06-10 12:03:22 +0400

    EFS mounted

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 apt-get update -y
 apt-get install -y apache2 libapache2-mod-php php-mysql php-mbstring 
+
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html/
