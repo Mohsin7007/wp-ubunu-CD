553e3689168ab5a043b37717bafa2c5c0ecdfcdc
Mohsin Nisar <mohsin@integratech.ae>
2021-06-10 12:48:07 +0400

    many changes mafe

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-chmod -R 755 /var/www/html/wordpress
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html/
+chmod -R 755 /var/www/html
