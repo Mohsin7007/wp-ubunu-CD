078ed60dee27280294f1c18e05cafce57ec10f9e
Mohsin Nisar <mohsin7007@hotmail.com>
2021-06-10 17:18:58 +0400

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 apt-get update -y
 apt-get install -y apache2 libapache2-mod-php php-mysql php-mbstring
+apt install mysql-client-core-8.0
 rm /var/www/html/index.html
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html
+
