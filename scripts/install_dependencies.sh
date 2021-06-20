207a3d5522d9da07a9c9eebb1cea911da292a7fb
Mohsin Nisar <mohsin7007@hotmail.com>
2021-06-20 19:10:06 +0400

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,7 +4,7 @@ apt-get install -y apache2 libapache2-mod-php php-mysql php-mbstring
 systemctl enable apache2
 apt install mysql-client-core-8.0
 rm /var/www/html/index.html
-sudo apt install nfs-common -y
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html
-grep -qxF 'fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html/ efs defaults,_netdev 0 0' /etc/fstab || echo 'fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html/ efs defaults,_netdev 0 0' >> /etc/fstab
+#sudo apt install nfs-common -y
+#sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html
+#grep -qxF 'fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html/ efs defaults,_netdev 0 0' /etc/fstab || echo 'fs-2879d473.efs.eu-central-1.amazonaws.com:/ /var/www/html/ efs defaults,_netdev 0 0' >> /etc/fstab
 
